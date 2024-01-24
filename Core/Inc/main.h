ba745ad7fc53e09325f46daa8a2e316033b217e7
Eric Navarrete <eric@ericdesk>
2024-01-24 23:35:11 +0000

    Initial Commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define DIV_EN_Pin GPIO_PIN_15
+#define DIV_EN_GPIO_Port GPIOC
+#define VBAT_MEAS_Pin GPIO_PIN_0
+#define VBAT_MEAS_GPIO_Port GPIOA
+#define LED_EN_Pin GPIO_PIN_3
+#define LED_EN_GPIO_Port GPIOA
+#define BMI_INT_1_Pin GPIO_PIN_7
+#define BMI_INT_1_GPIO_Port GPIOA
+#define BMI_INT_2_Pin GPIO_PIN_8
+#define BMI_INT_2_GPIO_Port GPIOA
+#define CHRG_Pin GPIO_PIN_11
+#define CHRG_GPIO_Port GPIOA
+#define EXT_RTC_INT_Pin GPIO_PIN_12
+#define EXT_RTC_INT_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
